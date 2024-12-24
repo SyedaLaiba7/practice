8e0244c9b189fe66da57f0c2bf18b1260f4131ee
SyedaLaiba7 <syedalaiba5645776@gmail.com>
2024-12-24 20:32:49 +0500

    my-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import Hero from "./components/Hero";
+
+import HomePage from "./HomePage/page";
 
 export default function Home() {
   return (
     <>
-    <Hero/>
+    <HomePage/>
     </>
   );
 }
